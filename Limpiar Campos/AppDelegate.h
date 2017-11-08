9bd255b467fccd2b56553f03aea46d908c0dcc08
Rafael Gaspar <rafaelgaspar@MacBook-Pro-de-Rafael.local>
2017-11-07 20:56:15 -0600

    Initial Commit

---
+++ Limpiar Campos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Limpiar Campos
+//
+//  Created by Rafael Gaspar on 07/11/17.
+//  Copyright © 2017 Rafael Gaspar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
